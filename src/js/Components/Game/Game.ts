5313d5a9290c35a938d01dba4aac94a5620c90c7
Kevin Rivera <kevinrev26@gmail.com>
2021-02-19 17:54:59 -0600

    Enable3D added to tool set

--- src/js/Components/Game/Game.ts
+++ src/js/Components/Game/Game.ts
@@ -1,50 +1,78 @@
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-//import Lama from "../../../assets/models/lama.gltf"
+// import * as THREE from 'three';
+// import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {ExtendedObject3D, Project, Scene3D} from "enable3d"
 
 // Main game class to render the canvas
 
-class Game {
+class Game extends Scene3D{
     
-    private height: number = window.innerHeight
-    private width: number = window.innerWidth
-    private renderer: THREE.WebGLRenderer
-    private scene: THREE.Scene
-    private camera: THREE.PerspectiveCamera
-    private object3D: THREE.Mesh
-    private controls: OrbitControls
+    // private height: number = window.innerHeight
+    // private width: number = window.innerWidth
+    // private renderer: THREE.WebGLRenderer
+    // private scene: THREE.Scene
+    // private camera: THREE.PerspectiveCamera
+    // private object3D: THREE.Mesh
+    // private controls: OrbitControls
+
+    private box: ExtendedObject3D | undefined 
 
     constructor(){
-
-        this.renderer = new THREE.WebGLRenderer()
-        this.renderer.setSize(this.width, this.height)
-        // orbit controls
-        this.scene = new THREE.Scene()
-        this.camera = new THREE.PerspectiveCamera(70, this.width/this.height,0.1,1000)
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-        this.object3D = this.addCube()
-
-        //Attach liseteners
-        // resize listener
-        document.body.appendChild(this.renderer.domElement)
+        super({key: "MainScene", enableXR: false})
     }
 
-    addCube(): THREE.Mesh {
-        const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
-        const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
-        const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
-        this.scene.add(cube)
-        this.camera.position.z = 2
-        return cube
+    async init(){
+        console.log("Preloading phase")
     }
 
-    public render(): void{
-        this.object3D.rotation.x += 0.01
-        this.object3D.rotation.y += 0.01
-        this.controls.update()
-        this.renderer.render(this.scene, this.camera)
+    async create(){
+        this.warpSpeed()
+
+        //this.physics.debug?.enable()
+
+        this.camera.position.set(10,10,20)
+
+        this.box = this.add.box({ y: 2 }, { lambert: { color: 'deepskyblue' } })
+        //this.physics.add.box({ y: 10 }, { lambert: { color: 'hotpink' } })
     }
+
+    update(){
+        if (this.box) {
+            this.box.rotation.x += 0.01
+            this.box.rotation.y += 0.01
+        }
+    }
+
+
+    // constructor(){
+
+    //     this.renderer = new THREE.WebGLRenderer()
+    //     this.renderer.setSize(this.width, this.height)
+    //     // orbit controls
+    //     this.scene = new THREE.Scene()
+    //     this.camera = new THREE.PerspectiveCamera(70, this.width/this.height,0.1,1000)
+    //     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    //     this.object3D = this.addCube()
+
+    //     //Attach liseteners
+    //     // resize listener
+    //     document.body.appendChild(this.renderer.domElement)
+    // }
+
+    // addCube(): THREE.Mesh {
+    //     const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
+    //     const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+    //     const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
+    //     this.scene.add(cube)
+    //     this.camera.position.z = 2
+    //     return cube
+    // }
+
+    // public render(): void{
+    //     this.object3D.rotation.x += 0.01
+    //     this.object3D.rotation.y += 0.01
+    //     this.controls.update()
+    //     this.renderer.render(this.scene, this.camera)
+    // }
 }
 
 export default Game
--- src/js/app.ts
+++ src/js/app.ts
@@ -1,18 +1,11 @@
 import Game from "./Components/Game/Game"
-
+import {Project} from "enable3d"
 
 
 const main = () => {
     console.log("Typescript setup first!")
 
-    const myGame: Game = new Game()
-
-    function animate(){
-        requestAnimationFrame(animate)
-        myGame.render()
-    }
-
-    animate()
+    const project = new Project({ scenes: [Game], antialias: true })
 } 
 
 window.onload = () => {
